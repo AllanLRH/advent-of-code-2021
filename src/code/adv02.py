f684d2abe3ba386d9d88815a62c2a337752ec708
Allan L. R. Hansen <allan.lrh@gmail.com>
2021-12-02 21:07:28 +0100

    Second part is done

--- src/code/adv02.py
+++ src/code/adv02.py
@@ -11,14 +11,31 @@ def read_data(pth: Path) -> list[tuple[str, int]]:
 
 
 def process_line_first(direction: str, magnitude: int) -> np.ndarray:
-    aim = np.zeros(2, dtype=int)
+    displacement = np.zeros(2, dtype=int)
     if direction == "forward":
-        aim[0] = magnitude
+        displacement[0] = magnitude
     elif direction == "down":
-        aim[1] = magnitude
+        displacement[1] = magnitude
     elif direction == "up":
-        aim[1] = -magnitude
-    return aim
+        displacement[1] = -magnitude
+    else:
+        raise ValueError(f"Invalid command {direction=}")
+    return displacement
+
+
+def resolve_path_second(data: list[tuple[str, int]]) -> tuple[int, int, int]:
+    aim, hor, dep = 0, 0, 0
+    for direction, v in data:
+        if direction == "down":
+            aim += v
+        elif direction == "up":
+            aim -= v
+        elif direction == "forward":
+            hor += v
+            dep += aim * v
+        else:
+            raise ValueError(f"Invalid command {direction=}")
+    return (aim, hor, dep)
 
 
 if __name__ == "__main__":
@@ -26,6 +43,11 @@ if __name__ == "__main__":
     data = read_data(data_path)
 
     processed_1 = np.asarray([process_line_first(*tup) for tup in data]).squeeze()
-    final_position = processed_1.sum(axis=0)
-    print(f"{final_position=}")
-    print(f"{final_position.prod()=}")
+    final_position_first = processed_1.sum(axis=0)
+    print(f"{final_position_first=}")
+    print(f"{final_position_first.prod()=}")
+
+    final_position_second = resolve_path_second(data)
+    answer = final_position_second[1] * final_position_second[2]
+    print(f"{final_position_second=}")
+    print(f"{answer=}")
